e7e42bcb13ce0132fad25caac5bdb73152428c1c
purgzz <crisburritos@gmail.com>
2025-10-17 20:12:39 -0700

    Add files via upload
    This is the front end of Campus Event Finder. Has mostly default arguments but I changed the main screen so that there is an input text box and a header stating what it is.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1> SDSU Campus Event Finder</h1>
+          
+          <input id="EventSearch" placeholder="Search Event" />
+      
+    </>
+  )
+}
+
+export default App
